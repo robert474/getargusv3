a7773bc4eacf3ee8c5eda3a19f4db496094d404e
SRidez_app <adibayubzai1@gmail.com>
2025-12-12 11:44:36 -0600

    next.js upgrade

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+interface CustomNextConfig extends NextConfig {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+    dirs?: string[];
+  };
+}
+
+const nextConfig: CustomNextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
